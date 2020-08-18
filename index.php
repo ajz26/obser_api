8dd5f908b0d96bda85dcd66e0f2148fe9c3995b7
Antonio <azambrano@tilomotion.com>
2020-08-18 17:27:51 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Obser.co
+ * Description: Funcionalidades personalizadas para obser
+ * Author: Antonio Zambrano
+ * Author URI: https://obser.co
+ * Version: 1.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+define('obser_plugin_dir', plugin_dir_path( __FILE__ ));
+define('obser_plugin_url', plugin_dir_url( __FILE__ ));
+
